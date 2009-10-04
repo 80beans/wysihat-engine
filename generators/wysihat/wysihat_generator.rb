408509e68d3972f33af83d1a2fc317370815aa0f
jeffkreeftmeijer <jeff@kreeftmeijer.nl>
2009-10-04 15:30:07 +0200

    added --no-stylesheet option to the generator

--- generators/wysihat/wysihat_generator.rb
+++ generators/wysihat/wysihat_generator.rb
@@ -1,51 +1,66 @@
 class WysihatGenerator < Rails::Generator::Base
+  default_options :stylesheet => true
+    
   def manifest
     record do |m|
-      
       m.file 'javascripts/wysihat.js',        'public/javascripts/wysihat.js'
       m.file 'javascripts/wysihat_engine.js', 'public/javascripts/wysihat_engine.js'
-      m.file 'javascripts/facebox.js',        'public/javascripts/facebox.js'
+
+      # facebox
       
-      m.file 'stylesheets/wysihat.css',       'public/stylesheets/wysihat.css'
       m.file 'stylesheets/facebox.css',       'public/stylesheets/facebox.css'
-      
-      m.file 'images/arrow_redo.png',         'public/images/arrow_redo.png'      
-      m.file 'images/arrow_undo.png',         'public/images/arrow_undo.png'      
+      m.file 'javascripts/facebox.js',        'public/javascripts/facebox.js'
       m.file 'images/b.png',                  'public/images/b.png'      
       m.file 'images/bl.png',                 'public/images/bl.png'      
       m.file 'images/br.png',                 'public/images/br.png'      
-      m.file 'images/closelabel.gif',         'public/images/closelabel.gif'      
-      m.file 'images/exclamation.png',        'public/images/exclamation.png'      
-      m.file 'images/film.png',               'public/images/film.png'      
-      m.file 'images/image.png',              'public/images/image.png'      
-      m.file 'images/link.png',               'public/images/link.png'      
-      m.file 'images/loading.gif',            'public/images/loading.gif'      
-      m.file 'images/page_code.png',          'public/images/page_code.png'      
-      m.file 'images/page_white_flash.png',   'public/images/page_white_flash.png'      
-      m.file 'images/paste_plain.png',        'public/images/paste_plain.png'      
-      m.file 'images/text_align_center.png',  'public/images/text_align_center.png'      
-      m.file 'images/text_align_left.png',    'public/images/text_align_left.png'      
-      m.file 'images/text_align_right.png',   'public/images/text_align_right.png'      
-      m.file 'images/text_bold.png',          'public/images/text_bold.png'      
-      m.file 'images/text_heading_1.png',     'public/images/text_heading_1.png'      
-      m.file 'images/text_heading_2.png',     'public/images/text_heading_2.png'      
-      m.file 'images/text_heading_3.png',     'public/images/text_heading_3.png'      
-      m.file 'images/text_italic.png',        'public/images/text_italic.png'      
-      m.file 'images/text_list_bullets.png',  'public/images/text_list_bullets.png'      
-      m.file 'images/text_list_numbers.png',  'public/images/text_list_numbers.png'      
-      m.file 'images/text_strikethrough.png', 'public/images/text_strikethrough.png'      
-      m.file 'images/text_underline.png',     'public/images/text_underline.png'      
+      m.file 'images/closelabel.gif',         'public/images/closelabel.gif'
       m.file 'images/tl.png',                 'public/images/tl.png'      
-      m.file 'images/tr.png',                 'public/images/tr.png'        
+      m.file 'images/tr.png',                 'public/images/tr.png'
       
+      # stylesheet & icons
+      
+      if options[:stylesheet]                           
+        m.file 'stylesheets/wysihat.css',       'public/stylesheets/wysihat.css'
+        
+        m.file 'images/arrow_redo.png',         'public/images/arrow_redo.png'      
+        m.file 'images/arrow_undo.png',         'public/images/arrow_undo.png'      
+        m.file 'images/exclamation.png',        'public/images/exclamation.png'      
+        m.file 'images/film.png',               'public/images/film.png'      
+        m.file 'images/image.png',              'public/images/image.png'      
+        m.file 'images/link.png',               'public/images/link.png'      
+        m.file 'images/loading.gif',            'public/images/loading.gif'      
+        m.file 'images/page_code.png',          'public/images/page_code.png'      
+        m.file 'images/page_white_flash.png',   'public/images/page_white_flash.png'      
+        m.file 'images/paste_plain.png',        'public/images/paste_plain.png'      
+        m.file 'images/text_align_center.png',  'public/images/text_align_center.png'      
+        m.file 'images/text_align_left.png',    'public/images/text_align_left.png'      
+        m.file 'images/text_align_right.png',   'public/images/text_align_right.png'      
+        m.file 'images/text_bold.png',          'public/images/text_bold.png'      
+        m.file 'images/text_heading_1.png',     'public/images/text_heading_1.png'      
+        m.file 'images/text_heading_2.png',     'public/images/text_heading_2.png'      
+        m.file 'images/text_heading_3.png',     'public/images/text_heading_3.png'      
+        m.file 'images/text_italic.png',        'public/images/text_italic.png'      
+        m.file 'images/text_list_bullets.png',  'public/images/text_list_bullets.png'      
+        m.file 'images/text_list_numbers.png',  'public/images/text_list_numbers.png'      
+        m.file 'images/text_strikethrough.png', 'public/images/text_strikethrough.png'      
+        m.file 'images/text_underline.png',     'public/images/text_underline.png'
+      end
+
       m.migration_template                    'migrations/create_wysihat_files.rb',
                                               'db/migrate',
                                               :migration_file_name => 'create_wysihat_files'
-      
+           
       m.route_resources :wysihat_files
       
       m.system 'script/plugin install git://github.com/markcatley/responds_to_parent.git'
       
     end
   end
+  
+  def add_options!(opt)
+    opt.separator ''
+    opt.separator 'Options:'
+    opt.on("--no-stylesheet", "Don't add the stylesheet file and don't copy the included icons.") { |v| options[:stylesheet] = false }
+  end
+    
 end
--- wysihat-engine.gemspec
+++ wysihat-engine.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{wysihat-engine}
-  s.version = "0.1.0"
+  s.version = "0.1.1"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Jeff Kreeftmeijer"]
-  s.date = %q{2009-10-03}
+  s.date = %q{2009-10-04}
   s.description = %q{A rails engine to allow users to use the WysiHat editors.}
   s.email = %q{jeff@80beans.com}
   s.extra_rdoc_files = [
