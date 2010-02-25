0e9330eeef8838f5180e24bf62fcf856a6471e03
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2010-02-25 13:29:33 +0100

    added uploaded file links and cleaned up the partial by moving stuff to a wysihat_files_helper.rb

--- app/helpers/wysihat_files_helper.rb
+++ app/helpers/wysihat_files_helper.rb
@@ -1,2 +1,13 @@
 module WysihatFilesHelper
+  def include_link(file)
+    function = file.file_content_type.starts_with?('image') ?
+      ".insertImage(this.href);" :
+      ".insertHTML('<a href=\"this.href\">#{file.file_file_name}</a>');"
+          
+    link_to_function(
+      file.file_file_name,
+      "$$('iframe##{params[:editor]}')[0]#{function}facebox.close();",
+      :href => file.file.url
+    )
+  end
 end
